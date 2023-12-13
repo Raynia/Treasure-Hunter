304e272f65b09cd5fe60b4866fad947207c2d989
Yurenia <46100945+Raynia@users.noreply.github.com>
2023-12-14 01:00:05 +0900

    Add 3 c++ class
    - celing lamp light
    - interactable actor
    - button connected door

---
+++ Source/TreasureHunter/THButtonConnectedDoor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "THInteractableAcor.h"
+#include "THButtonConnectedDoor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TREASUREHUNTER_API ATHButtonConnectedDoor : public ATHInteractableAcor
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TreasureHunter/THInteractableActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "THInteractableAcor.h"
+
+// Sets default values
+ATHInteractableAcor::ATHInteractableAcor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATHInteractableAcor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATHInteractableAcor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
